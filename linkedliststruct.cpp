66ede3fc2b197a9cce390bb325abceb2901f2da6
Ramprasath-0103 <ramprasath.rpurusothaman@gmail.com>
2025-08-05 14:10:18 +0530

    fix : converting an array to linked list

--- linkedliststruct.cpp
+++ linkedliststruct.cpp
@@ -13,11 +13,29 @@ public:
         data = data1;
         next = next1;
     }
-};
 
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+Node *convertArr2LL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
 int main()
 {
     vector<int> arr = {2, 5, 8, 7, 1, 3, 4, 6};
-    Node *y = new Node(arr[0], nullptr);
-    cout << y->data << endl;
+    Node *head = convertArr2LL(arr);
+    cout << head->data << endl;
 }
